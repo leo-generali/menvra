9054e2b45fe4fa4379b84bec3e7b632d546219a5
Leo Generali <leogen17@gmail.com>
2018-10-10 23:05:04 -0400

    more test

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const next = require('next');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
-const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
